c8809434cd8503831a6cc85a8ef90da596f67532
belladonna-peru <miguelsolcas7@gmail.com>
2025-06-20 16:41:36 -0500

    Actualizar script.js

--- script.js
+++ script.js
@@ -267,6 +267,4 @@ function cambiarVista(vista) {
   cont.classList.toggle("list", vista === 'list');
 }
 
-.productos.grid .producto { width: calc(33% - 20px); }
-.productos.list .producto { display: flex; }
 
